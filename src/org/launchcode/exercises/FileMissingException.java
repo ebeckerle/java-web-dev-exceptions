428db609a6216d9bfc94f6def94a0beb9c8923e7
Elizabeth <elzbthyorke@gmail.com>
2022-05-05 09:56:32 -0500

    "11.2 - Exercises : Exceptions"

---
+++ src/org/launchcode/exercises/FileMissingException.java
@@ -0,0 +1,8 @@
+package org.launchcode.exercises;
+
+public class FileMissingException extends Exception {
+
+    public FileMissingException (String message){
+        super(message);
+    }
+}
--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -1,13 +1,15 @@
 package org.launchcode.exercises;
 
+import org.launchcode.Temperature.TemperatureException;
+
 import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
 
         // Test out your Divide() function!
+//        Divide(4, 0);
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -16,16 +18,37 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        CheckFileExtension(studentFiles.get("Carl"));
+        CheckFileExtension(studentFiles.get("Brad"));
     }
 
-    public static void Divide(int x, int y)
+    public static int Divide(int x, int y)
     {
         // Write code here!
+        if (y == 0){
+            try {
+                throw new ArithmeticException("You cannot divide by zero!");
+            } catch (ArithmeticException e){
+                e.printStackTrace();
+            }
+        }
+        return x/y;
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
+    public static int CheckFileExtension(String fileName) {
+        int grade = 0;
+        if (fileName == null || fileName.equals("")){
+            try {
+                throw new FileMissingException("No file submitted");
+            }catch (FileMissingException e){
+                e.printStackTrace();
+            }
+        } else if (fileName.contains(".java")){
+            grade = 1;
+        } else {
+            grade = -1;
+        }
+        return grade;
     }
 
 }
